74f30c5a726d090232d555e9ec536cc1fdbc3c62
Alex Axthelm <Alex@2degrees-investing.org>
2019-11-20 16:10:22 -0500

    Python changes to allow bootstrapping database

--- twodii_datawarehouse.py
+++ twodii_datawarehouse.py
@@ -1,12 +1,36 @@
+#!/usr/bin/python
+"""Update database and import data.
+
+This is the main application file. running this file will bootstrap or update
+the data warehouse database, and import any data files which need imported
+"""
+
+
+# External imports
 import psycopg2
 
-import twodii_datawarehouse
+
+# Project imports
+import twodii_datawarehouse.migrations as migrations
+
 
 def main():
-    """Main entrypoint for bootstrapping database and importing data files"""
+    """Run database migrations and import data."""
     # This will generate the db connection (from envvars), find current
     # version, run new migrations, and load any data
-    pass
+    print("Establishing db connection")
+    db_connection = psycopg2.connect(
+        dbname='twodii',
+        user='postgres',
+        password='postgres',
+        host='db'
+    )
+
+    print("Section 1: Migrations")
+    migrations.run_migrations(db_connection)
+
+    db_connection.close()
+
 
 if __name__ == '__main__':
     main()
--- twodii_datawarehouse/migrations.py
+++ twodii_datawarehouse/migrations.py
@@ -1,14 +1,96 @@
+"""Update structure of data warehouse database.
+
+This module houses python code for running SQL migrations in the data
+warehouse, including bootstrapping an empty database.
+"""
+
+import psycopg2.extras
+
+METADATA_SCHEMA = 'public'
+# MIGRATION_HISTORY_TABLE = 'foo'
+MIGRATION_HISTORY_TABLE = 'migration_history'
 
 
 def run_migrations(
     db_connection,
-    migrations_path="./sql"
+    migrations_path='./sql'
 ):
-    "Run all migrations that haven't already been run against the database"
-    pass
+    """Bootstrap and Update database structure.
+
+    When run, it will determine the current data warehouse version, and run any
+    additional migrations, if needed. If the data warehouse has not been
+    initialized, it will bootstrap the data warehouse with initialization
+    scripts.
+    """
+    dw_version = get_dw_version(db_connection)
+    dw_current_version = 0
+    if dw_version is None:
+        print("No Data warehouse found. Bootstrapping.")
+        with db_connection.cursor() as cursor:
+            cursor.execute(open("./sql/000_001_000.sql", "r").read())
+    else:
+        print(f"""Data Warehouse at version {dw_version}.
+        Updating to {dw_current_version}""")
 
 
 def get_dw_version(db_connection):
-    """Query the database to determine which migrations have already been run
-    against it."""
-    pass
+    """Determine which migrations have already been run against DB.
+
+    returns a dict-like object (from a psycopg2 dict-like cursor)
+    This will first run a check that the data warehouse has been initialized
+    (and contains the migration history table). If it has, then it will
+    determine the current version recorded int the migration history table, and
+    report that.
+    """
+    print("Finding current dw_version")
+
+    existence_check_query = f"""
+    SELECT
+        table_name
+    FROM information_schema.tables
+    WHERE table_schema = %(schema)s
+    AND table_name = %(table)s
+    LIMIT 1;
+    """
+
+    cur = db_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute(
+        existence_check_query,
+        {'schema': METADATA_SCHEMA, 'table': MIGRATION_HISTORY_TABLE}
+    )
+    table_exists = cur.fetchone()
+    db_connection.rollback()
+
+    if table_exists is None:
+        return None
+
+    # Passing the name of a table is the only time we want to use string
+    # interpolation to pass paramaters. It's okay here because its interpolated
+    # to a static parameter.
+    max_version_query = f"""
+    SELECT
+        major,
+        minor,
+        patch
+    FROM {METADATA_SCHEMA}.{MIGRATION_HISTORY_TABLE}
+    ORDER BY
+        major DESC,
+        minor DESC,
+        patch DESC
+    LIMIT 1;
+    """
+
+    cur.execute(max_version_query)
+    max_version = cur.fetchone()
+    db_connection.rollback()
+
+    cur.close()
+    return max_version
+
+
+# def run_migrations(
+#     db_connection,
+#     migrations_path="./sql"
+# ):
+#     "Run all migrations that haven't already been run against the database"
+#     pass
