b3918670536a9b4daddc498c5798d247d53cdcfc
Alex Axthelm <Alex@2degrees-investing.org>
2019-11-26 13:42:54 -0500

    add functions for reading sql column info
    Also a function to check if the table exists in the DB.

--- app/twodii_datawarehouse/file_import/utils.py
+++ app/twodii_datawarehouse/file_import/utils.py
@@ -1,4 +1,6 @@
 """Utility functions for file import."""
+import pandas as pd
+import pandas.io.sql as sqlio
 
 
 def find_header_row(
@@ -46,7 +48,7 @@ def clean_df_header(
     columns_name_list,
     **kwargs
 ):
-    """Cleans the header and all rows above from a dataframe."""
+    """Clean the header and all rows above from a dataframe."""
     header_row, header_names = find_header_row(
         df,
         columns_name_list,
@@ -56,6 +58,7 @@ def clean_df_header(
     df.columns = header_names
     # include the header row in the rows to drop
     df = df.drop(range(header_row + 1))
+    return df
 
 
 def find_df_footer(
@@ -65,9 +68,123 @@ def find_df_footer(
     """Find the row in which a dataframe footer starts.
 
     rows_to_search: the number of rows in which a valid footer. None will
-    search all rows"""
+    search all rows.
+    """
     if rows_to_search is None:
         rows_to_search = df.shape[0]
+    footer_start = None
     for k, v in df.tail(rows_to_search).iterrows():
-        # TODO: iterate over row contents, and ensure that all are
-        # pd.isnan(thing)
+        if all(pd.isna(v.values)):
+            footer_start = k
+            break
+    return footer_start
+
+
+def clean_df_footer(df, **kwargs):
+    """Clean the footer from a dataframe (anything after an empty line)."""
+    footer_start = find_df_footer(df, **kwargs)
+    if footer_start:
+        remove_index = range(footer_start, df.index[-1] + 1)
+        df = df.drop(remove_index)
+    return df
+
+
+def check_table_exists_in_db(
+    db_connection,
+    tablename,
+    schemaname='rawdata',
+    raise_exception=True
+):
+    """Check that the table exists in the database.
+
+    raise_exception: if True, will raise and exception if the table is not
+    found in the database.
+    """
+    query = """
+    SELECT
+        table_name
+    FROM information_schema.tables
+    WHERE table_name = %(tablename)s
+    AND table_schema = %(schemaname)s
+    """
+    table_info = sqlio.read_sql(
+        sql=query,
+        con=db_connection,
+        params={'tablename': tablename, 'schemaname': schemaname}
+    )
+    if raise_exception & (len(table_info) == 0):
+        raise Exception(f"Table {schemaname}.{tablename} not in database.")
+    return len(table_info) == 1
+
+
+def get_db_column_info(
+    db_connection,
+    tablename,
+    schemaname='rawdata'
+):
+    """Find the column names and types for a table."""
+    # Before starting, check that the table exists
+    check_table_exists_in_db(
+        db_connection=db_connection,
+        tablename=tablename,
+        schemaname=schemaname,
+        raise_exception=True
+    )
+    query = """
+        SELECT
+            col.column_name,
+            col.ordinal_position,
+            col.is_nullable,
+            col.data_type,
+            tc.constraint_type
+        FROM information_schema.columns AS col
+            LEFT JOIN information_schema.key_column_usage AS ccu ON (
+                col.column_name = ccu.column_name
+                AND col.table_name = ccu.table_name
+                AND col.table_schema = ccu.table_schema
+                AND col.table_catalog = ccu.table_catalog
+            )
+            LEFT JOIN information_schema.table_constraints AS tc ON (
+                ccu.constraint_name = tc.constraint_name
+                AND ccu.constraint_catalog = tc.constraint_catalog
+            )
+        WHERE col.table_name = %(tablename)s
+        AND col.table_schema = %(schemaname)s
+        ORDER BY ordinal_position
+    """
+    col_info = sqlio.read_sql(
+        sql=query,
+        con=db_connection,
+        params={'tablename': tablename, 'schemaname': schemaname}
+    )
+    if len(col_info) == 0:
+        raise Exception(f"No column info found for {schemaname}.{tablename}")
+    return col_info
+
+
+def write_df_to_db(
+    df,
+    db_connection,
+    tablename,
+    schemaname='rawdata'
+):
+    """Write the contents of a dataframe to the database."""
+    target_column_info = get_db_column_info(
+        db_connection=db_connection,
+        tablename=tablename,
+        schemaname=schemaname
+    )
+    target_columns = list(target_column_info['column_name'])
+    bad_columns = [x for x in df.columns if x not in target_columns]
+    if len(bad_columns):
+        err_msg = ["Unexpected column names could not map to DB columns:"]
+        err_msg.extend(bad_columns)
+        raise Exception("\n\t".join(err_msg))
+
+
+"""
+
+importlib.reload(utils)
+utils.write_df_to_db(df, db_connection, 'globaldata_power_plants')
+
+"""
