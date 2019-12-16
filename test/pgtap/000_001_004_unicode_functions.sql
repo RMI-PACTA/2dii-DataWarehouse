ef38b812258fb723d05ed3987ab2c50002e1f92d
Alex Axthelm <Alex@2degrees-investing.org>
2019-12-16 13:33:13 -0500

    Add pre-simplified flag to unicode tests

--- test/pgtap/000_001_004_unicode_functions.sql
+++ test/pgtap/000_001_004_unicode_functions.sql
@@ -6,163 +6,166 @@ BEGIN;
     codepoint TEXT,
     unicode TEXT,
     romanized TEXT,
-    simplified TEXT
+    simplified TEXT,
+    pre_simplified BOOLEAN
   );
 
-  INSERT INTO unicode_tests (codepoint, unicode, romanized, simplified) VALUES
-  /* ('0009', '"', NULL, NULL), */
-  /* ('0020', '2', NULL, NULL), */	 
-  /* ('0021', '!', NULL, NULL), */
-  /* ('0022', '"', NULL, NULL), */
-  /* ('0023', '#', NULL, NULL), */
-  /* ('0024', '$', NULL, NULL), */
-  /* ('0025', '%', NULL, NULL), */
-  /* ('0026', '&', NULL, NULL), */
-  /* ('0027', ''', NULL, NULL), */
-  /* ('0028', '(', NULL, NULL), */
-  /* ('0029', ')', NULL, NULL), */
-  /* ('002A', '*', NULL, NULL), */
-  /* ('002B', '+', NULL, NULL), */
-  /* ('002C', ',', NULL, NULL), */
-  /* ('002D', '-', NULL, NULL), */
-  /* ('002E', '.', NULL, NULL), */
-  /* ('002F', '/', NULL, NULL), */
-  /* ('0030', '0', NULL, NULL), */
-  /* ('0031', '1', NULL, NULL), */
-  /* ('0032', '2', NULL, NULL), */
-  /* ('0033', '3', NULL, NULL), */
-  /* ('0034', '4', NULL, NULL), */
-  /* ('0035', '5', NULL, NULL), */
-  /* ('0036', '6', NULL, NULL), */
-  /* ('0037', '7', NULL, NULL), */
-  /* ('0038', '8', NULL, NULL), */
-  /* ('0039', '9', NULL, NULL), */
-  /* ('003A', ':', NULL, NULL), */
-  /* ('003B', ';', NULL, NULL), */
-  /* ('003C', '<', NULL, NULL), */
-  /* ('003E', '>', NULL, NULL), */
-  /* ('003F', '?', NULL, NULL), */
-  /* ('0040', '@', NULL, NULL), */
-  /* ('0041', 'A', NULL, NULL), */
-  /* ('0042', 'B', NULL, NULL), */
-  /* ('0043', 'C', NULL, NULL), */
-  /* ('0044', 'D', NULL, NULL), */
-  /* ('0045', 'E', NULL, NULL), */
-  /* ('0046', 'F', NULL, NULL), */
-  /* ('0047', 'G', NULL, NULL), */
-  /* ('0048', 'H', NULL, NULL), */
-  /* ('0049', 'I', NULL, NULL), */
-  /* ('004A', 'J', NULL, NULL), */
-  /* ('004B', 'K', NULL, NULL), */
-  /* ('004C', 'L', NULL, NULL), */
-  /* ('004D', 'M', NULL, NULL), */
-  /* ('004E', 'N', NULL, NULL), */
-  /* ('004F', 'O', NULL, NULL), */
-  /* ('0050', 'P', NULL, NULL), */
-  /* ('0051', 'Q', NULL, NULL), */
-  /* ('0052', 'R', NULL, NULL), */
-  /* ('0053', 'S', NULL, NULL), */
-  /* ('0054', 'T', NULL, NULL), */
-  /* ('0055', 'U', NULL, NULL), */
-  /* ('0056', 'V', NULL, NULL), */
-  /* ('0057', 'W', NULL, NULL), */
-  /* ('0058', 'X', NULL, NULL), */
-  /* ('0059', 'Y', NULL, NULL), */
-  /* ('005A', 'Z', NULL, NULL), */
-  /* ('005B', '[', NULL, NULL), */
-  /* ('005C', '\', NULL, NULL), */
-  /* ('005D', ']', NULL, NULL), */
-  /* ('005F', '_', NULL, NULL), */
-  /* ('0060', '`', NULL, NULL), */
-  /* ('0061', 'a', NULL, NULL), */
-  /* ('0062', 'b', NULL, NULL), */
-  /* ('0063', 'c', NULL, NULL), */
-  /* ('0064', 'd', NULL, NULL), */
-  /* ('0065', 'e', NULL, NULL), */
-  /* ('0066', 'f', NULL, NULL), */
-  /* ('0067', 'g', NULL, NULL), */
-  /* ('0068', 'h', NULL, NULL), */
-  /* ('0069', 'i', NULL, NULL), */
-  /* ('006A', 'j', NULL, NULL), */
-  /* ('006B', 'k', NULL, NULL), */
-  /* ('006C', 'l', NULL, NULL), */
-  /* ('006D', 'm', NULL, NULL), */
-  /* ('006E', 'n', NULL, NULL), */
-  /* ('006F', 'o', NULL, NULL), */
-  /* ('0070', 'p', NULL, NULL), */
-  /* ('0071', 'q', NULL, NULL), */
-  /* ('0072', 'r', NULL, NULL), */
-  /* ('0073', 's', NULL, NULL), */
-  /* ('0074', 't', NULL, NULL), */
-  /* ('0075', 'u', NULL, NULL), */
-  /* ('0076', 'v', NULL, NULL), */
-  /* ('0077', 'w', NULL, NULL), */
-  /* ('0078', 'x', NULL, NULL), */
-  /* ('0079', 'y', NULL, NULL), */
-  /* ('007A', 'z', NULL, NULL), */
-  /* ('007C', '|', NULL, NULL), */
-  /* ('009D', '', NULL, NULL), */
-  ('00A0', 'foo bar', 'foo bar', 'foo bar'),
-  ('00A0', ' ', ' ', ' '),
-  /* ('00A0', ' ', NULL, NULL), */
-  /* ('00A2', '¢', NULL, NULL), */
-  /* ('00A6', '¦', NULL, NULL), */
-  /* ('00AA', 'ª', NULL, NULL), */
-  /* ('00AD', '­', NULL, NULL), */
-  /* ('00B0', '°', NULL, NULL), */
-  /* ('00B2', '²', NULL, NULL), */
-  /* ('00B4', '´', NULL, NULL), */
-  /* ('00B7', '·', NULL, NULL), */
-  /* ('00BA', 'º', NULL, NULL), */
-  /* ('00BD', '½', NULL, NULL), */
-  /* ('00BE', '¾', NULL, NULL), */
-  /* ('00C2', 'Â', NULL, NULL), */
-  /* ('00C3', 'Ã', NULL, NULL), */
-  /* ('00C5', 'Å', NULL, NULL), */
-  /* ('00C7', 'Ç', NULL, NULL), */
-  /* ('00C9', 'É', NULL, NULL), */
-  /* ('00D6', 'Ö', NULL, NULL), */
-  /* ('00DC', 'Ü', NULL, NULL), */
-  /* ('00DF', 'ß', NULL, NULL), */
-  /* ('00E1', 'á', NULL, NULL), */
-  /* ('00E2', 'â', NULL, NULL), */
-  /* ('00E3', 'ã', NULL, NULL), */
-  ('00E4', 'Jyväskylä', 'Jyvaeskylae', 'jyvaeskylae'),
-  ('00E4', 'ä', 'ae', 'ae'),
-  /* ('00E4', 'ä', NULL, NULL), */
-  /* ('00E6', 'æ', NULL, NULL), */
-  /* ('00E7', 'ç', NULL, NULL), */
-  /* ('00E8', 'è', NULL, NULL), */
-  /* ('00E9', 'é', NULL, NULL), */
-  /* ('00EA', 'ê', NULL, NULL), */
-  /* ('00EC', 'ì', NULL, NULL), */
-  /* ('00ED', 'í', NULL, NULL), */
-  /* ('00EE', 'î', NULL, NULL), */
-  /* ('00EF', 'ï', NULL, NULL), */
-  /* ('00F0', 'ð', NULL, NULL), */
-  /* ('00F1', 'ñ', NULL, NULL), */
-  /* ('00F3', 'ó', NULL, NULL), */
-  /* ('00F4', 'ô', NULL, NULL), */
-  /* ('00F5', 'õ', NULL, NULL), */
-  /* ('00F6', 'ö', NULL, NULL), */
-  /* ('00F8', 'ø', NULL, NULL), */
-  /* ('00FA', 'ú', NULL, NULL), */
-  /* ('00FC', 'ü', NULL, NULL), */
-  /* ('00FE', 'þ', NULL, NULL), */
-  /* ('017D', 'Ž', NULL, NULL), */
-  /* ('0192', 'ƒ', NULL, NULL), */
-  /* ('2013', '–', NULL, NULL), */
-  /* ('2014', '—', NULL, NULL), */
-  /* ('2018', '‘', NULL, NULL), */
-  /* ('2019', '’', NULL, NULL), */
-  /* ('201A', '‚', NULL, NULL), */
-  /* ('201C', '“', NULL, NULL), */
-  /* ('201D', '”', NULL, NULL), */
-  /* ('201E', '„', NULL, NULL), */
-  /* ('2026', '…', NULL, NULL), */
-  ('20AC', '€', 'EUR', 'eur'),
-  ('20AC', '€9001', 'EUR9001', 'eur9001'),
-  ('20AC', 'Sw€€t', 'SwEUREURt', 'sweureurt')
+  INSERT INTO unicode_tests (
+    codepoint, unicode, romanized, simplified, pre_simplified
+  ) VALUES
+  /* ('0009', '"', NULL, NULL, false), */
+  /* ('0020', '2', NULL, NULL, false), */	 
+  /* ('0021', '!', NULL, NULL, false), */
+  /* ('0022', '"', NULL, NULL, false), */
+  /* ('0023', '#', NULL, NULL, false), */
+  /* ('0024', '$', NULL, NULL, false), */
+  /* ('0025', '%', NULL, NULL, false), */
+  /* ('0026', '&', NULL, NULL, false), */
+  /* ('0027', ''', NULL, NULL, false), */
+  /* ('0028', '(', NULL, NULL, false), */
+  /* ('0029', ')', NULL, NULL, false), */
+  /* ('002A', '*', NULL, NULL, false), */
+  /* ('002B', '+', NULL, NULL, false), */
+  /* ('002C', ',', NULL, NULL, false), */
+  /* ('002D', '-', NULL, NULL, false), */
+  /* ('002E', '.', NULL, NULL, false), */
+  /* ('002F', '/', NULL, NULL, false), */
+  /* ('0030', '0', NULL, NULL, false), */
+  /* ('0031', '1', NULL, NULL, false), */
+  /* ('0032', '2', NULL, NULL, false), */
+  /* ('0033', '3', NULL, NULL, false), */
+  /* ('0034', '4', NULL, NULL, false), */
+  /* ('0035', '5', NULL, NULL, false), */
+  /* ('0036', '6', NULL, NULL, false), */
+  /* ('0037', '7', NULL, NULL, false), */
+  /* ('0038', '8', NULL, NULL, false), */
+  /* ('0039', '9', NULL, NULL, false), */
+  /* ('003A', ':', NULL, NULL, false), */
+  /* ('003B', ';', NULL, NULL, false), */
+  /* ('003C', '<', NULL, NULL, false), */
+  /* ('003E', '>', NULL, NULL, false), */
+  /* ('003F', '?', NULL, NULL, false), */
+  /* ('0040', '@', NULL, NULL, false), */
+  /* ('0041', 'A', NULL, NULL, false), */
+  /* ('0042', 'B', NULL, NULL, false), */
+  /* ('0043', 'C', NULL, NULL, false), */
+  /* ('0044', 'D', NULL, NULL, false), */
+  /* ('0045', 'E', NULL, NULL, false), */
+  /* ('0046', 'F', NULL, NULL, false), */
+  /* ('0047', 'G', NULL, NULL, false), */
+  /* ('0048', 'H', NULL, NULL, false), */
+  /* ('0049', 'I', NULL, NULL, false), */
+  /* ('004A', 'J', NULL, NULL, false), */
+  /* ('004B', 'K', NULL, NULL, false), */
+  /* ('004C', 'L', NULL, NULL, false), */
+  /* ('004D', 'M', NULL, NULL, false), */
+  /* ('004E', 'N', NULL, NULL, false), */
+  /* ('004F', 'O', NULL, NULL, false), */
+  /* ('0050', 'P', NULL, NULL, false), */
+  /* ('0051', 'Q', NULL, NULL, false), */
+  /* ('0052', 'R', NULL, NULL, false), */
+  /* ('0053', 'S', NULL, NULL, false), */
+  /* ('0054', 'T', NULL, NULL, false), */
+  /* ('0055', 'U', NULL, NULL, false), */
+  /* ('0056', 'V', NULL, NULL, false), */
+  /* ('0057', 'W', NULL, NULL, false), */
+  /* ('0058', 'X', NULL, NULL, false), */
+  /* ('0059', 'Y', NULL, NULL, false), */
+  /* ('005A', 'Z', NULL, NULL, false), */
+  /* ('005B', '[', NULL, NULL, false), */
+  /* ('005C', '\', NULL, NULL, false), */
+  /* ('005D', ']', NULL, NULL, false), */
+  /* ('005F', '_', NULL, NULL, false), */
+  /* ('0060', '`', NULL, NULL, false), */
+  /* ('0061', 'a', NULL, NULL, false), */
+  /* ('0062', 'b', NULL, NULL, false), */
+  /* ('0063', 'c', NULL, NULL, false), */
+  /* ('0064', 'd', NULL, NULL, false), */
+  /* ('0065', 'e', NULL, NULL, false), */
+  /* ('0066', 'f', NULL, NULL, false), */
+  /* ('0067', 'g', NULL, NULL, false), */
+  /* ('0068', 'h', NULL, NULL, false), */
+  /* ('0069', 'i', NULL, NULL, false), */
+  /* ('006A', 'j', NULL, NULL, false), */
+  /* ('006B', 'k', NULL, NULL, false), */
+  /* ('006C', 'l', NULL, NULL, false), */
+  /* ('006D', 'm', NULL, NULL, false), */
+  /* ('006E', 'n', NULL, NULL, false), */
+  /* ('006F', 'o', NULL, NULL, false), */
+  /* ('0070', 'p', NULL, NULL, false), */
+  /* ('0071', 'q', NULL, NULL, false), */
+  /* ('0072', 'r', NULL, NULL, false), */
+  /* ('0073', 's', NULL, NULL, false), */
+  /* ('0074', 't', NULL, NULL, false), */
+  /* ('0075', 'u', NULL, NULL, false), */
+  /* ('0076', 'v', NULL, NULL, false), */
+  /* ('0077', 'w', NULL, NULL, false), */
+  /* ('0078', 'x', NULL, NULL, false), */
+  /* ('0079', 'y', NULL, NULL, false), */
+  /* ('007A', 'z', NULL, NULL, false), */
+  /* ('007C', '|', NULL, NULL, false), */
+  /* ('009D', '', NULL, NULL, false), */
+  ('00A0', 'foo bar', 'foo bar', 'foo bar', false),
+  ('00A0', ' ', ' ', ' ', false),
+  /* ('00A0', ' ', NULL, NULL, false), */
+  /* ('00A2', '¢', NULL, NULL, false), */
+  /* ('00A6', '¦', NULL, NULL, false), */
+  /* ('00AA', 'ª', NULL, NULL, false), */
+  /* ('00AD', '­', NULL, NULL, false), */
+  /* ('00B0', '°', NULL, NULL, false), */
+  /* ('00B2', '²', NULL, NULL, false), */
+  /* ('00B4', '´', NULL, NULL, false), */
+  /* ('00B7', '·', NULL, NULL, false), */
+  /* ('00BA', 'º', NULL, NULL, false), */
+  /* ('00BD', '½', NULL, NULL, false), */
+  /* ('00BE', '¾', NULL, NULL, false), */
+  /* ('00C2', 'Â', NULL, NULL, false), */
+  /* ('00C3', 'Ã', NULL, NULL, false), */
+  /* ('00C5', 'Å', NULL, NULL, false), */
+  /* ('00C7', 'Ç', NULL, NULL, false), */
+  /* ('00C9', 'É', NULL, NULL, false), */
+  /* ('00D6', 'Ö', NULL, NULL, false), */
+  /* ('00DC', 'Ü', NULL, NULL, false), */
+  /* ('00DF', 'ß', NULL, NULL, false), */
+  /* ('00E1', 'á', NULL, NULL, false), */
+  /* ('00E2', 'â', NULL, NULL, false), */
+  /* ('00E3', 'ã', NULL, NULL, false), */
+  ('00E4', 'Jyväskylä', 'Jyvaeskylae', 'jyvaeskylae', false),
+  ('00E4', 'ä', 'ae', 'ae', false),
+  /* ('00E4', 'ä', NULL, NULL, false), */
+  /* ('00E6', 'æ', NULL, NULL, false), */
+  /* ('00E7', 'ç', NULL, NULL, false), */
+  /* ('00E8', 'è', NULL, NULL, false), */
+  /* ('00E9', 'é', NULL, NULL, false), */
+  /* ('00EA', 'ê', NULL, NULL, false), */
+  /* ('00EC', 'ì', NULL, NULL, false), */
+  /* ('00ED', 'í', NULL, NULL, false), */
+  /* ('00EE', 'î', NULL, NULL, false), */
+  /* ('00EF', 'ï', NULL, NULL, false), */
+  /* ('00F0', 'ð', NULL, NULL, false), */
+  /* ('00F1', 'ñ', NULL, NULL, false), */
+  /* ('00F3', 'ó', NULL, NULL, false), */
+  /* ('00F4', 'ô', NULL, NULL, false), */
+  /* ('00F5', 'õ', NULL, NULL, false), */
+  /* ('00F6', 'ö', NULL, NULL, false), */
+  /* ('00F8', 'ø', NULL, NULL, false), */
+  /* ('00FA', 'ú', NULL, NULL, false), */
+  /* ('00FC', 'ü', NULL, NULL, false), */
+  /* ('00FE', 'þ', NULL, NULL, false), */
+  /* ('017D', 'Ž', NULL, NULL, false), */
+  /* ('0192', 'ƒ', NULL, NULL, false), */
+  /* ('2013', '–', NULL, NULL, false), */
+  /* ('2014', '—', NULL, NULL, false), */
+  /* ('2018', '‘', NULL, NULL, false), */
+  /* ('2019', '’', NULL, NULL, false), */
+  /* ('201A', '‚', NULL, NULL, false), */
+  /* ('201C', '“', NULL, NULL, false), */
+  /* ('201D', '”', NULL, NULL, false), */
+  /* ('201E', '„', NULL, NULL, false), */
+  /* ('2026', '…', NULL, NULL, false), */
+  ('20AC', '€', 'EUR', 'eur', false),
+  ('20AC', '€9001', 'EUR9001', 'eur9001', false),
+  ('20AC', 'Sw€€t', 'SwEUREURt', 'sweureurt', false)
   ;
 
   SELECT plan(COUNT(*)::INT * 8) FROM unicode_tests;
