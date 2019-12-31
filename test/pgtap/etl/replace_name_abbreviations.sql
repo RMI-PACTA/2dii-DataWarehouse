de9fb94e3d60f50e3c80de9b88911ada2f719982
Alex Axthelm <Alex@2degrees-investing.org>
2019-12-30 19:45:47 -0500

    Add tests for name replacement

--- test/pgtap/etl/replace_name_abbreviations.sql
+++ test/pgtap/etl/replace_name_abbreviations.sql
@@ -1,6 +1,5 @@
 BEGIN;
-
-  CREATE TEMPORARY TABLE name_abbreviation_tests (
+CREATE TEMPORARY TABLE name_abbreviation_tests (
     test_string TEXT UNIQUE NOT NULL,
     expected TEXT NOT NULL,
     description TEXT
@@ -19,6 +18,8 @@ BEGIN;
   ('bandcamp', 'bandcamp', '''and'' in middle of word not replaced'),
   ('band camp', 'band camp', '''and'' at end of word not replaced'),
   ('robot android', 'robot android', '''and'' at beginning of word not replaced'),
+  ('and more', 'and more', '''and'' at beginning of string not replaced (~* ^and\s)'),
+  ('foo and', 'foo and', '''and'' at end of string not replaced (~* \sand$)'),
   /* \s+en\s+ */
   ('dit en dat', 'dit & dat', '''en'' to ampersand'),
   ('DIT EN DAT', 'DIT & DAT', '''en'' to ampersand'),
@@ -26,6 +27,8 @@ BEGIN;
   ('dit	en  dat', 'dit & dat', '''en'' with spacings'),
   ('ten thousand', 'ten thousand', '''en'' at end of word not replaced'),
   ('The End', 'The End', '''en'' at beginning of word not replaced'),
+  ('en more', 'en more', '''en'' at beginning of string not replaced (~* ^en\s)'),
+  ('foo en', 'foo en', '''en'' at end of string not replaced (~* \sen$)'),
   /* \s+och\s+ */
   ('A och B', 'A & B', '''och'' to ampersand'),
   ('A OCH B', 'A & B', '''och'' case-insensitive'),
@@ -33,6 +36,8 @@ BEGIN;
   ('Sochi', 'Sochi', '''och'' in middle of word not replaced'),
   ('tengo ocho llamas', 'tengo ocho llamas', '''och'' at beginning of word not replaced'),
   ('Loch Ness', 'Loch Ness', '''och'' at end of word not replaced'),
+  ('och more', 'och more', '''och'' at beginning of string not replaced (~* ^och\s)'),
+  ('foo och', 'foo och', '''och'' at ochd of string not replaced (~* \soch$)'),
   /* \s+und\s+ */
   ('A und B', 'A & B', '''und'' to ampersand'),
   ('A UND B', 'A & B', '''und'' case-insensitive'),
@@ -40,6 +45,8 @@ BEGIN;
   ('Bundesliga', 'Bundesliga', '''und'' in middle of word not replaced'),
   ('towed under', 'towed under', '''und'' at beginning of word not replaced'),
   ('fund manager', 'fund manager', '''und'' at end of word not replaced'),
+  ('und more', 'und more', '''und'' at beginning of string not replaced (~* ^und\s)'),
+  ('foo und', 'foo und', '''und'' at undd of string not replaced (~* \sund$)'),
   /* ampersand */
   ('this & that', 'this & that', 'ampersand does not change'),
   /* (inactive) */
@@ -54,22 +61,39 @@ BEGIN;
   ('foobar-aktg', 'foobar-ag', NULL),
   ('Foobar AKTG', 'Foobar$ag', NULL),
   ('foo aktg bar', 'foo ag bar', NULL),
+  ('baraktgfoo', 'baragfoo', NULL),
   /* aktiengesellschaft */
   ('foobar aktiengesellschaft', 'foobar$ag', NULL),
   ('foobar-aktiengesellschaft', 'foobar-ag', NULL),
   ('Foobar AKTIENGESELLSCHAFT', 'Foobar$ag', NULL),
   ('Foobar Aktiengesellschaft', 'Foobar$ag', NULL),
   ('foo aktiengesellschaft bar', 'foo ag bar', NULL),
-  ('foobar ag', 'foobar$ag', '''ag'' does not reduce'),
-  ('foo ag bar', 'foo ag bar', '''ag'' does not reduce'),
+  ('baraktiengesellschaftfoo', 'baragfoo', NULL),
   /* associate */
+  ('testing associate', 'testing assoc', '''associate'' reduces to assoc'),
+  ('Testing Associate', 'Testing assoc', NULL),
+  ('foo, bar, & associate', 'foo, bar, & assoc', NULL),
+  ('Foo, Bar, & Associate', 'Foo, Bar, & assoc', NULL),
+  ('Associated Media', 'assocd Media', '''associated'' reduces to assocd'),
+  ('foo associate bar', 'foo assoc bar', NULL),
   /* associates */
   ('testing associates', 'testing assoc', '''associates'' reduces to assoc'),
   ('Testing Associates', 'Testing assoc', NULL),
   ('foo, bar, & associates', 'foo, bar, & assoc', NULL),
   ('Foo, Bar, & Associates', 'Foo, Bar, & assoc', NULL),
   /* berhad */
+  ('foobar berhad', 'foobar$bhd', NULL),
+  ('Foobar Berhad', 'Foobar$bhd', NULL),
+  ('foo berhad bar', 'foo bhd bar', NULL),
+  ('fooberhadbar', 'foobhdbar', NULL),
   /* company */
+  ('foobar company', 'foobar$co', NULL),
+  ('FooBar Company', 'FooBar$co', NULL),
+  ('foo company bar', 'foo co bar', NULL),
+  ('Foo Company Bar', 'Foo co Bar', NULL),
+  ('Company Foobar', 'co Foobar', NULL),
+  ('Foo BarCompany', 'Foo Barco', '~* company, !~* \scompany'),
+  ('foo barcompany', 'foo barco', NULL),
   /* corporation */
   /* designated\s+activity\s+company */
   /* develop */
@@ -93,12 +117,29 @@ BEGIN;
   /* resources */
   /* shipping */
   /* \s+ag$ */
+  ('foobar ag', 'foobar$ag', NULL),
+  ('FooBar AG', 'FooBar$ag', NULL),
+  ('FooBar	AG', 'FooBar$ag', 'tab whitespace'),
+  ('big bag', 'big bag', 'ag suffix must have space before.'),
+  ('foo ag bar', 'foo ag bar', NULL),
+  ('foobar ag ag', 'foobar ag$ag', NULL),
   /* \s+as$ */
   /* \s+asa$ */
   /* \s+bhd$ */
+  ('Foobar bhd', 'Foobar$bhd', NULL),
+  ('Foobar Bhd', 'Foobar$bhd', NULL),
+  ('Foobar	Bhd', 'Foobar$bhd', 'tab whitespace'),
+  ('foobhdbar', 'foobhdbar', NULL),
+  ('foo bhd bar', 'foo bhd bar', NULL),
   /* \s+bsc$ */
   /* \s+bv$ */
   /* \s+co$ */
+  ('Foobar co', 'Foobar$co', NULL),
+  ('Foobar Co', 'Foobar$co', NULL),
+  ('Foobar	Co', 'Foobar$co', 'tab whitespace'),
+  ('Foobar & Co', 'Foobar &$co', 'tab whitespace'),
+  ('foocobar', 'foocobar', NULL),
+  ('foo co bar', 'foo co bar', NULL),
   /* \s+corp$ */
   /* \s+cv$ */
   /* \s+dac$ */
@@ -136,7 +177,7 @@ BEGIN;
   SELECT is(
     etl.replace_name_abbreviations(test_string),
     expected,
-    COALESCE(description || ': ', '') ||
+    COALESCE(quote_literal(description) || ': ', '') ||
       quote_literal(test_string) || ' -> ' ||
       quote_literal(expected)
   ) FROM name_abbreviation_tests;
