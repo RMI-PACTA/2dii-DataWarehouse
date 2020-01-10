930e25ed0340aa3551c742913982cafd8fcc8b67
Alex Axthelm <Alex@2degrees-investing.org>
2020-01-10 12:49:43 -0500

    change names for test dataframes

--- test/pytest/test_file_import_utils.py
+++ test/pytest/test_file_import_utils.py
@@ -6,7 +6,7 @@ import pytest
 import twodii_datawarehouse.file_import.utils as utils
 
 # Test frame with header on first row, no footers
-df_test_1a = pd.DataFrame([
+df_test_simple = pd.DataFrame([
     ["intA", "strB", "floatC", "intD", "strE", "floatF"],
     [4, "foo", 1.23, 1, "", 4.8],
     [3, "bar", 1.33, 2, "stringX", 4.8],
@@ -17,7 +17,7 @@ df_test_1a = pd.DataFrame([
 ])
 
 # Test frame with header on first row, no footers
-df_test_1b = pd.DataFrame([
+df_test_small_header = pd.DataFrame([
     ["Sample", "", "", "", "", ""],
     ["", "Header", "", "", "", ""],
     ["", "", "Info", "", "", ""],
@@ -31,7 +31,7 @@ df_test_1b = pd.DataFrame([
 ])
 
 # Test frame with header on first row, no footers
-df_test_1c = pd.DataFrame([
+df_test_long_header = pd.DataFrame([
     ["0", "Sample", "", "", "", ""],
     ["1", "Header", "", "", "", ""],
     ["2", "Info", "", "", "", ""],
@@ -54,7 +54,7 @@ df_test_1c = pd.DataFrame([
 ])
 
 # Test frame with header on first row, no footers
-df_test_1d = pd.DataFrame([
+df_test_multiple_headers = pd.DataFrame([
     ["intA", "strB", "", "", "", ""],
     [4, "foo", 1.23, 1, "", 4.8],
     [3, "bar", 1.33, 2, "stringX", 4.8],
@@ -66,14 +66,14 @@ df_test_1d = pd.DataFrame([
     [4, "", 1.63, 1, "", ""]
 ])
 
-df_test_1_names = ["intA", "strB", "floatC", "intD", "strE", "floatF"]
+df_test_names = ["intA", "strB", "floatC", "intD", "strE", "floatF"]
 
 
 # ---------------------------- find_header_row ----------------------------
 def test_find_simple_header_top_row():
     header_row, header_names = utils.find_header_row(
-        df=df_test_1a,
-        columns_name_list=df_test_1_names
+        df=df_test_simple,
+        columns_name_list=df_test_names
     )
     assert header_row == 0
     assert header_names == ["inta", "strb", "floatc", "intd", "stre", "floatf"]
@@ -81,8 +81,8 @@ def test_find_simple_header_top_row():
 
 def test_find_simple_header_fourth_row():
     header_row, header_names = utils.find_header_row(
-        df=df_test_1b,
-        columns_name_list=df_test_1_names
+        df=df_test_small_header,
+        columns_name_list=df_test_names
     )
     assert header_row == 3
     assert header_names == ["inta", "strb", "floatc", "intd", "stre", "floatf"]
@@ -91,8 +91,8 @@ def test_find_simple_header_fourth_row():
 def test_find_simple_header_throw_exception_default_rows():
     with pytest.raises(Exception) as excinfo:
         utils.find_header_row(
-            df=df_test_1c,
-            columns_name_list=df_test_1_names
+            df=df_test_long_header,
+            columns_name_list=df_test_names
         )
     assert str(excinfo.value) == "Header not found in 10 rows"
 
@@ -100,8 +100,8 @@ def test_find_simple_header_throw_exception_default_rows():
 def test_find_simple_header_throw_exception_limited_rows():
     with pytest.raises(Exception) as excinfo:
         utils.find_header_row(
-            df=df_test_1c,
-            columns_name_list=df_test_1_names,
+            df=df_test_long_header,
+            columns_name_list=df_test_names,
             rows_to_search=5
         )
     assert str(excinfo.value) == "Header not found in 5 rows"
@@ -110,8 +110,8 @@ def test_find_simple_header_throw_exception_limited_rows():
 def test_find_simple_header_throw_exception_partial_match():
     with pytest.raises(Exception) as excinfo:
         utils.find_header_row(
-            df=df_test_1d,
-            columns_name_list=df_test_1_names,
+            df=df_test_multiple_headers,
+            columns_name_list=df_test_names,
             rows_to_search=3
         )
     assert str(excinfo.value) == "Header not found in 3 rows"
@@ -120,8 +120,8 @@ def test_find_simple_header_throw_exception_partial_match():
 def test_find_simple_header_search_exact_rows():
     with pytest.raises(Exception) as excinfo:
         utils.find_header_row(
-            df=df_test_1c,
-            columns_name_list=df_test_1_names,
+            df=df_test_long_header,
+            columns_name_list=df_test_names,
             rows_to_search=12
         )
     assert str(excinfo.value) == "Header not found in 12 rows"
@@ -129,8 +129,8 @@ def test_find_simple_header_search_exact_rows():
 
 def test_find_simple_header_rows_to_test_15():
     header_row, header_names = utils.find_header_row(
-        df=df_test_1c,
-        columns_name_list=df_test_1_names,
+        df=df_test_long_header,
+        columns_name_list=df_test_names,
         rows_to_search=15
     )
     assert header_row == 12
@@ -139,8 +139,8 @@ def test_find_simple_header_rows_to_test_15():
 
 def test_find_simple_header_rows_to_test_none():
     header_row, header_names = utils.find_header_row(
-        df=df_test_1c,
-        columns_name_list=df_test_1_names,
+        df=df_test_long_header,
+        columns_name_list=df_test_names,
         rows_to_search=None
     )
     assert header_row == 12
@@ -149,8 +149,8 @@ def test_find_simple_header_rows_to_test_none():
 
 def test_stop_at_first_header_row_above_threshold_default():
     header_row, header_names = utils.find_header_row(
-        df=df_test_1d,
-        columns_name_list=df_test_1_names
+        df=df_test_multiple_headers,
+        columns_name_list=df_test_names
     )
     assert header_row == 4
     assert header_names == ["inta", "strb", "floatc", "intd", "", ""]
@@ -158,8 +158,8 @@ def test_stop_at_first_header_row_above_threshold_default():
 
 def test_stop_at_first_header_row_above_threshold_value():
     header_row, header_names = utils.find_header_row(
-        df=df_test_1d,
-        columns_name_list=df_test_1_names,
+        df=df_test_multiple_headers,
+        columns_name_list=df_test_names,
         stop_threshold=(1/6)
     )
     assert header_row == 0
@@ -168,8 +168,8 @@ def test_stop_at_first_header_row_above_threshold_value():
 
 def test_find_best_header_in_range():
     header_row, header_names = utils.find_header_row(
-        df=df_test_1d,
-        columns_name_list=df_test_1_names,
+        df=df_test_multiple_headers,
+        columns_name_list=df_test_names,
         stop_threshold=1
     )
     assert header_row == 7
@@ -179,8 +179,8 @@ def test_find_best_header_in_range():
 def test_find_header_row_stop_threshold_must_be_numeric():
     with pytest.raises(TypeError) as excinfo:
         utils.find_header_row(
-            df=df_test_1d,
-            columns_name_list=df_test_1_names,
+            df=df_test_multiple_headers,
+            columns_name_list=df_test_names,
             stop_threshold=None
         )
     assert "'>' not supported" in str(excinfo.value)
@@ -189,8 +189,8 @@ def test_find_header_row_stop_threshold_must_be_numeric():
 # ---------------------------- clean_header ----------------------------
 def test_clean_simple_header():
     clean_df = utils.clean_df_header(
-        df=df_test_1d,
-        columns_name_list=df_test_1_names
+        df=df_test_multiple_headers,
+        columns_name_list=df_test_names
     )
     npt.assert_array_equal(
         clean_df,
@@ -209,8 +209,8 @@ def test_clean_simple_header():
 
 def test_clean_header_pass_kwargs_rows():
     clean_df = utils.clean_df_header(
-        df=df_test_1c,
-        columns_name_list=df_test_1_names,
+        df=df_test_long_header,
+        columns_name_list=df_test_names,
         rows_to_search=15
     )
     npt.assert_array_equal(
@@ -232,8 +232,8 @@ def test_clean_header_pass_kwargs_rows():
 
 def test_clean_header_pass_kwargs_threshold():
     clean_df = utils.clean_df_header(
-        df=df_test_1d,
-        columns_name_list=df_test_1_names,
+        df=df_test_multiple_headers,
+        columns_name_list=df_test_names,
         stop_threshold=1
     )
     npt.assert_array_equal(
@@ -258,12 +258,10 @@ df_test_footer_quote = pd.DataFrame([
     ["", "foo", 1.53, "", "stringZ", 0.8],
     [4, "", 1.63, 1, "", ""],
     ["", "", "", "", "", ""],
-    ["", "", "", "", "", ""],
     ["Footer", "text", "", "", "", ""],
 ])
 
 
-# Test frame with header on first row, and footer
 df_test_footer_nan = pd.DataFrame([
     ["intA", "strB", "floatC", "intD", "strE", "floatF"],
     [4, "foo", 1.23, 1, "", 4.8],
@@ -273,12 +271,10 @@ df_test_footer_nan = pd.DataFrame([
     ["", "foo", 1.53, "", "stringZ", 0.8],
     [4, "", 1.63, 1, "", ""],
     [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
-    [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
     ["Footer", "text", "", "", "", ""],
 ])
 
 
-# Test frame with header on first row, and footer
 df_test_footer_mixed = pd.DataFrame([
     ["intA", "strB", "floatC", "intD", "strE", "floatF"],
     [4, "foo", 1.23, 1, "", 4.8],
@@ -288,13 +284,11 @@ df_test_footer_mixed = pd.DataFrame([
     ["", "foo", 1.53, "", "stringZ", 0.8],
     [4, "", 1.63, 1, "", ""],
     ["", np.nan, "", np.nan, "", np.nan],
-    [np.nan, "", np.nan, "", np.nan, ""],
     ["Footer", "text", "", "", "", ""],
 ])
 
 
-# Test frame with header on first row, and footer
-df_test_2_long_footer = pd.DataFrame([
+df_test_long_footer = pd.DataFrame([
     ["intA", "strB", "floatC", "intD", "strE", "floatF"],
     [4, "foo", 1.23, 1, "", 4.8],
     [3, "bar", 1.33, 2, "stringX", 4.8],
@@ -302,6 +296,7 @@ df_test_2_long_footer = pd.DataFrame([
     [8, "bax", "", -1, "", ""],
     ["", "foo", 1.53, "", "stringZ", 0.8],
     [4, "", 1.63, 1, "", ""],
+    ["", "", "", "", "", ""],
     ["", "", "", "", "", ""],
     ["Long", "1", "", "", "", ""],
     ["Footer", "2", "", "", "", ""],
@@ -352,21 +347,21 @@ def test_find_simple_footer_mixed_nan():
 
 def test_find_no_footer():
     footer_row = utils.find_df_footer(
-        df=df_test_1a
+        df=df_test_simple
     )
     assert footer_row is None
 
 
 def test_find_long_footer_default():
     footer_row = utils.find_df_footer(
-        df=df_test_2_long_footer
+        df=df_test_long_footer
     )
     assert footer_row is None
 
 
 def test_find_long_footer_none():
     footer_row = utils.find_df_footer(
-        df=df_test_2_long_footer,
+        df=df_test_long_footer,
         rows_to_search=None
     )
     assert footer_row == 7
@@ -374,10 +369,10 @@ def test_find_long_footer_none():
 
 def test_find_long_footer_value():
     footer_row = utils.find_df_footer(
-        df=df_test_2_long_footer,
+        df=df_test_long_footer,
         rows_to_search=25
     )
-    assert footer_row  == 7
+    assert footer_row == 7
 
 
 # ---------------------------- clean_df_footer ----------------------------
@@ -385,50 +380,50 @@ def test_clean_simple_footer():
     clean_df = utils.clean_df_footer(
         df=df_test_footer_quote
     )
-    npt.assert_array_equal(clean_df, df_test_1a)
+    npt.assert_array_equal(clean_df, df_test_simple)
 
 
 def test_clean_simple_footer_nan():
     clean_df = utils.clean_df_footer(
         df=df_test_footer_nan
     )
-    npt.assert_array_equal(clean_df, df_test_1a)
+    npt.assert_array_equal(clean_df, df_test_simple)
 
 
 def test_clean_simple_footer_mixed_nan():
     clean_df = utils.clean_df_footer(
         df=df_test_footer_mixed
     )
-    npt.assert_array_equal(clean_df, df_test_1a)
+    npt.assert_array_equal(clean_df, df_test_simple)
 
 
 def test_clean_no_footer():
     clean_df = utils.clean_df_footer(
-        df=df_test_1a
+        df=df_test_simple
     )
-    npt.assert_array_equal(clean_df, df_test_1a)
+    npt.assert_array_equal(clean_df, df_test_simple)
 
 
 def test_clean_long_footer_default():
     clean_df = utils.clean_df_footer(
-        df=df_test_2_long_footer
+        df=df_test_long_footer
     )
     # since the footer doesn't find_df_footert in the default parameters,
     # nothing gets cleaned.
-    npt.assert_array_equal(clean_df, df_test_2_long_footer)
+    npt.assert_array_equal(clean_df, df_test_long_footer)
 
 
 def test_clean_long_footer_none():
     clean_df = utils.clean_df_footer(
-        df=df_test_2_long_footer,
+        df=df_test_long_footer,
         rows_to_search=None
     )
-    npt.assert_array_equal(clean_df, df_test_1a)
+    npt.assert_array_equal(clean_df, df_test_simple)
 
 
 def test_clean_long_footer_value():
     clean_df = utils.clean_df_footer(
-        df=df_test_2_long_footer,
+        df=df_test_long_footer,
         rows_to_search=25
     )
-    npt.assert_array_equal(clean_df, df_test_1a)
+    npt.assert_array_equal(clean_df, df_test_simple)
