d5753901d51ffc2592c0d567b14f70ccc97491f5
Alex Axthelm <Alex@2degrees-investing.org>
2020-01-17 01:13:32 +0100

    Finish setting up transaction fixtures

--- test/python_unit/file_import/test_db_utils.py
+++ test/python_unit/file_import/test_db_utils.py
@@ -1,4 +1,5 @@
 """Tests for database utilities for file importing."""
+import logging
 import os
 import pytest
 import sqlalchemy as sqla
@@ -24,35 +25,86 @@ def db_engine(connection_string):
     return sqla.create_engine(connection_string)
 
 
-# @pytest.fixture
-# def db_transact(db_engine):
-#     """Create a transaction which cleans up after itself by rolling back"""
-#     with db_engine.connect().begin() as db_con:
-#         yield db_con
-#         db_con.rollback()
+@pytest.fixture
+def db_transact(request, db_engine):
+    """Create a transaction which cleans up after itself by rolling back"""
+    connection = db_engine.connect()
+    transaction = connection.begin()
+    logging.debug("Creating transaction")
+
+    def rollback_transaction():
+        logging.debug("Rolling back transaction")
+        transaction.rollback()
+
+    request.addfinalizer(rollback_transaction)
+    # Returning the transaction with connection intact
+    return connection
 
 
-# def teardown_function():
-#     print('tearing down')
-
-
-def test_check_table_exists_schema_does_not_exist(db_engine):
+def test_check_table_exists_schema_does_not_exist_raise(db_engine):
     with pytest.raises(Exception) as excinfo:
         dbu.check_table_exists_in_db(
             db_connection=db_engine,
             tablename="tablebar",
-            schemaname='schemafoo',
-            raise_exception=True
+            schemaname='schemafoo'
         )
     assert str(excinfo.value) == "Table schemafoo.tablebar not in database."
 
 
-def test_check_table_exists_table_does_not_exist(db_engine):
+def test_check_table_exists_table_does_not_exist_raise(db_engine):
     with pytest.raises(Exception) as excinfo:
         dbu.check_table_exists_in_db(
             db_connection=db_engine,
             tablename="tablebar",
-            schemaname="public",
-            raise_exception=True
+            schemaname="public"
         )
     assert str(excinfo.value) == "Table public.tablebar not in database."
+
+
+def test_check_table_exists_schema_does_not_exist_value(db_engine):
+    table_found = dbu.check_table_exists_in_db(
+        db_connection=db_engine,
+        tablename="tablebar",
+        schemaname='schemafoo',
+        raise_exception=False
+    )
+    assert table_found is False
+
+
+def test_check_table_exists_table_does_not_exist_value(db_engine):
+    table_found = dbu.check_table_exists_in_db(
+        db_connection=db_engine,
+        tablename="tablebar",
+        schemaname="public",
+        raise_exception=False
+    )
+    assert table_found is False
+
+
+# The information_schema.columns table always exists on postgres
+def test_check_table_exists_table_exist_engine_raise(db_engine):
+    table_found = dbu.check_table_exists_in_db(
+        db_connection=db_engine,
+        tablename="columns",
+        schemaname="information_schema"
+    )
+    assert table_found is True
+
+
+def test_check_table_exists_table_exist_engine_value(db_engine):
+    table_found = dbu.check_table_exists_in_db(
+        db_connection=db_engine,
+        tablename="columns",
+        schemaname="information_schema",
+        raise_exception=False
+    )
+    assert table_found is True
+
+
+def test_check_table_exists_table_exist_engine_default(db_engine):
+    table_found = dbu.check_table_exists_in_db(
+        db_connection=db_engine,
+        tablename="columns",
+        schemaname="information_schema",
+    )
+    assert table_found is True
